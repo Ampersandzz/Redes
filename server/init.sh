2e511aff2a16c1a83ca1aa951d943a9b9a7f993a
Ampersand <sebastian.cerda@mail.udp.cl>
2020-09-30 13:58:00 -0400

    fixed query

--- server/init.sh
+++ server/init.sh
@@ -4,6 +4,6 @@ TEMP_FILE='/var/tmp/mysql-user.sql'
 cat > "$TEMP_FILE" << EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'some_pass';
 CREATE USER 'redes'@'%' IDENTIFIED BY 'some_pass';
-GRANT ALL PRIVILEGES ON *.* TO 'redes'@'%'
+GRANT ALL PRIVILEGES ON *.* TO 'redes'@'%';
 ALTER USER 'redes'@'%' IDENTIFIED WITH mysql_native_password BY 'some_pass';
 EOF
