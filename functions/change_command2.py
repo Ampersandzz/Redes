7741c3937493d04090b8a28663ed7baa0706fe9f
Ampersand <sebastian.cerda@mail.udp.cl>
2020-10-16 19:05:03 -0400

    added change_command function

--- functions/change_command2.py
+++ functions/change_command2.py
@@ -1,6 +1,6 @@
 def change_command(packet):
     if packet['IP']['src'] == '172.17.0.3' and packet['MYSQL']['packet_length'] > 0:
         print(packet['MYSQL']['command'])
-        packet['MYSQL']['command'] = 5
+        packet['MYSQL']['command'] = 1
         print(packet['MYSQL']['command'])
     return packet
