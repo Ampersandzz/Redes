462790085266f37cc231e4a64ed428d6ccd2881d
Yui Zhou <yui.zhou97@gmail.com>
2020-10-16 23:34:50 -0300

    Update change_length.py

--- functions/change_length.py
+++ functions/change_length.py
@@ -1,6 +1,6 @@
 def change_length(packet):
     if packet['IP']['src'] == '172.17.0.3':
-        print(packet['IP']['len'], packet['MYSQL']['packet_length'])
+        print(packet['MYSQL']['packet_length'])
         packet['MYSQL']['packet_length'] = 57
-        print(packet['IP']['len'], packet['MYSQL']['packet_length'])
+        print(packet['MYSQL']['packet_length'])
     return packet
