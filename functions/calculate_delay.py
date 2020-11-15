83f70b5a9c366b8ddd3ab24bb1568874fc313989
Ampersand <sebastian.cerda@mail.udp.cl>
2020-11-15 17:27:17 -0500

    modified calculate_delay function

--- functions/calculate_delay.py
+++ functions/calculate_delay.py
@@ -2,12 +2,23 @@ def calculate_delay(packet):
     from datetime import datetime
     if packet['IP']['src'] == '172.17.0.3' and packet['MYSQL']:
         try:
-            if packet.time:
-                packet.time = datetime.now()
+            if packet.server_time:
+                packet.server_time = datetime.now()
         except:
             pass
-        packet.global_var('time', datetime.now())
+        packet.global_var('server_time', datetime.now())
     elif packet['IP']['src'] == '172.17.0.2' and packet['MYSQL']:
-        delay = (datetime.now() - packet.time).microseconds * 2 /1000
-        print("{}ms".format(delay))
+        delay = (datetime.now() - packet.server_time).microseconds/1000
+        try:
+            if packet.server_delay:
+                packet.server_delay = delay
+            if packet.client_time:
+                packet.client_time = datetime.now()
+        except:
+            pass
+        packet.global_var('server_delay', delay)
+        packet.global_var('client_time', datetime.now())
+    if len(packet) == 52:
+        delay2 = (datetime.now() - packet.client_time).microseconds/1000
+        print('tiempo en obtener respuesta del servidor: {}ms'.format(delay2+packet.server_delay))
     return packet
